05c725731a65532a00d9edf3786d52803635d56c
nam <namsupermen06@gmail.com>
2023-10-08 15:46:29 +0700

    delete file unnecessary

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,14 @@
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger-output.json";
 
-import DepartmentRoute from "./DepartmentRoute";
-import CompanyRoute from "./CompanyRoute";
-import UnitRoute from "./UnitRoute";
-import SupplierRoute from "./SupplierRoute";
-import OriginRoute from "./OriginRoute";
-import GroupProductRoute from "./GroupProductRoute";
 import ProductRoute from "./ProductRoute";
-import DocumentRoute from "./DocumentRoute";
 import AuthRoute from "./AuthRoute";
 import { notFound } from "../http/middlewares/handle_error";
 
 const initRoutes = (app) => {
   app.use("/api/v1/auth", AuthRoute);
-  app.use("/api/v1/department", DepartmentRoute);
-  app.use("/api/v1/company", CompanyRoute);
-  app.use("/api/v1/unit", UnitRoute);
-  app.use("/api/v1/supplier", SupplierRoute);
-  app.use("/api/v1/origin", OriginRoute);
-  app.use("/api/v1/group-product", GroupProductRoute);
+  app.use("/api/v1/group-category", ProductRoute);
   app.use("/api/v1/product", ProductRoute);
-  app.use("/api/v1/document", DocumentRoute);
 
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
