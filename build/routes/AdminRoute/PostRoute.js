ce4a6d9d56d33289f78096e781092cafce65d6a4
namnd <namnd@rabiloo.com>
2023-11-02 11:47:44 +0700

    fix wrong name of folder

--- build/routes/AdminRoute/PostRoute.js
+++ build/routes/AdminRoute/PostRoute.js
@@ -7,7 +7,7 @@ exports["default"] = void 0;
 var _express = _interopRequireDefault(require("express"));
 var _cloudinaryConfig = _interopRequireDefault(require("../../config/cloudinaryConfig"));
 var _PostRequest = _interopRequireDefault(require("../../http/requests/PostRequest"));
-var _uploadRequest = _interopRequireDefault(require("../../http/requests/uploadRequest"));
+var _UploadRequest = _interopRequireDefault(require("../../http/requests/UploadRequest"));
 var _PostController = _interopRequireDefault(require("../../http/controllers/AdminController/PostController"));
 var _verifyToken = require("../../http/middlewares/verifyToken");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
@@ -18,7 +18,7 @@ router.get("/getAllStatus", _PostController["default"].getAllStatus);
 // router.get("/export-excel", PostController.exportExcel);
 router.use(_verifyToken.verifyAccessToken);
 router["delete"]("/:id", _PostController["default"].destroy);
-router.post("/upload-photo", [_cloudinaryConfig["default"].single("photo"), _uploadRequest["default"]], _PostController["default"].uploadPhoto);
+router.post("/upload-photo", [_cloudinaryConfig["default"].single("photo"), _UploadRequest["default"]], _PostController["default"].uploadPhoto);
 router.use(_PostRequest["default"]);
 router.post("/", _cloudinaryConfig["default"].single("photo"), _PostController["default"].create);
 router.put("/:id", _cloudinaryConfig["default"].single("photo"), _PostController["default"].update);
