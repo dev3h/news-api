204a38ca2f83112e573addbaea630cbaee45c95c
namnd <namnd@rabiloo.com>
2023-11-02 11:43:16 +0700

    fix find folder when build

--- build/routes/index.js
+++ build/routes/index.js
@@ -6,14 +6,15 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var _swaggerUiExpress = _interopRequireDefault(require("swagger-ui-express"));
 var _swaggerOutput = _interopRequireDefault(require("../swagger-output.json"));
-var _AuthRoute = require("./AuthRoute");
 var _generateError = require("../helpers/generateError");
 var _AdminRoute = _interopRequireDefault(require("./AdminRoute"));
 var _UserRoute = _interopRequireDefault(require("./UserRoute"));
+var _AdminAuthRoute = _interopRequireDefault(require("./AuthRoute/AdminAuthRoute"));
+var _UserAuthRoute = _interopRequireDefault(require("./AuthRoute/UserAuthRoute"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var initRoutes = function initRoutes(app) {
-  (0, _AuthRoute.AdminAuthRoute)(app);
-  (0, _AuthRoute.UserAuthRoute)(app);
+  (0, _AdminAuthRoute["default"])(app);
+  (0, _UserAuthRoute["default"])(app);
   (0, _AdminRoute["default"])(app);
   (0, _UserRoute["default"])(app);
   app.use("/api-docs", _swaggerUiExpress["default"].serve, _swaggerUiExpress["default"].setup(_swaggerOutput["default"]));
