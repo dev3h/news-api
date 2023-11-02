ad812a7a808df3a5970fa7cd657c01b83a134198
namnd <namnd@rabiloo.com>
2023-11-02 10:43:39 +0700

    fix config to database for production

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,17 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, null, {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  logging: false,
-  timezone: "+07:00",
-});
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+    timezone: "+07:00",
+  }
+);
 
 const connectDB = async () => {
   try {
